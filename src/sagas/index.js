0856b74edfb6a8d5f15d3528159cd8b308799485
Shumeiko <TShumeiko@luxoft.com>
2021-08-20 02:31:15 +0300

    add saga

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,99 +1,46 @@
-// import { eventChannel } from 'redux-saga';
-// import { call, fork, put, take } from 'redux-saga/effects';
-// import io from 'socket.io-client';
-//
-// const socket = io('wss://api-pub.bitfinex.com/ws/2');
-//
-// const SocketEvents = {
-//   event: "subscribe",
-//   channel: "book",
-//   symbol: "tBTCUSD",
-// };
-//
-// const createLiveTickerChannel = () => {
-//   const subscribe = emitter => {
-//     socket.on(SocketEvents, emitter);
-//     return () => socket.removeListener(SocketEvents, emitter);
-//   };
-//   return eventChannel(subscribe);
-// }
-//
-// function* liveTickerSaga() {
-//   const channel = yield call(createLiveTickerChannel);
-//   while (true) {
-//     const jobs = yield take(channel);
-//     console.log('jobs', jobs);
-//     // const action = JobsActions.fresh(jobs);
-//     // yield put(action);
-//   }
-// }
-//
-// export function* saga() {
-//   yield fork(liveTickerSaga);
-// }
-
-
-import { all, apply, call, fork, take } from 'redux-saga/effects';
+import { call, put, take } from 'redux-saga/effects';
 import { eventChannel } from 'redux-saga';
 
-// function createSocketConnection() {
-//   return new WebSocket('wss://api-pub.bitfinex.com/ws/2');
-// }
+const payload = {
+  event: "subscribe",
+  channel: "book",
+  symbol: "tBTCUSD",
+};
 
-const socket = new WebSocket('wss://api-pub.bitfinex.com/ws/2')
-
-const createSocketChannel = ws =>
+const createSocketChannel = () =>
   eventChannel(emitter => {
-    ws.addEventListener('message', message => {
-      socket.send({
-        event: "subscribe", channel: "book", symbol: "tBTCUSD"
-      })
-      console.log('message.data', message.data);
-    });
+    const ws = new WebSocket('wss://api-pub.bitfinex.com/ws/2');
 
-    ws.addEventListener('open', () => {
-      socket.send({
-        event: "subscribe", channel: "book", symbol: "tBTCUSD"
-      })
-      console.log('open');
-    });
+    ws.onopen = () => {
+      ws.send(JSON.stringify(payload));
+    };
 
-    ws.addEventListener('error', () => {
-      console.log('error');
-    });
+    ws.onmessage = message => {
+      let msg;
+      try {
+        msg = JSON.parse(message.data);
+      } catch (error) {
+        console.error(`Error parsing: ${error}`);
+      }
+      if (msg) {
+        emitter( { type: 'ACTION_TYPE', payload: msg } );
+      }
+    };
 
-    ws.addEventListener('close', () => {
-      console.log('close');
-    });
+    ws.onerror = error => {
+      console.log(`WebSocket error: ${error}`);
+    }
 
-    return () => ws.close();
+    // unsubscribe function
+    return () => {
+      console.log('Socket off');
+    };
   });
 
-function* emitResponse(socket) {
-  yield apply(socket, socket.emit, ['message received']);
-}
-
-function* watchSocketChannel() {
-  // const socket = yield call(createSocketConnection, 'http://localhost:3000', 'wss://api-pub.bitfinex.com/ws/2');
-  const socketChannel = yield call(createSocketChannel, socket);
-
-  console.log('socket', socket);
-  console.log('socketChannel', socketChannel);
-
-  while (true) {
-    try {
-      const payload = yield take(socketChannel);
-      console.log('payload', payload);
-      // yield put({type: actions.WEBSOCKET_MESSAGE, payload});
-      yield fork(emitResponse, socket);
-    } catch (err) {
-      console.log('socket error: ', err);
-    }
-  }
-}
-
 export function* saga() {
-  yield all([
-    fork(watchSocketChannel),
-  ])
+  const channel = yield call(createSocketChannel);
+  while (true) {
+    const action = yield take(channel);
+    yield put(action);
+  }
 }
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,3 +1,12 @@
-export const reducer = () => {
+const initialState = {};
 
+export const reducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+    case 'ACTION_TYPE':
+      console.log('payload', payload);
+      return {};
+
+    default:
+      return state;
+  }
 };
